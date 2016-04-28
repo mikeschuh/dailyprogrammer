122f445fc51fed18ee2c5333027b99c044b4c6fb
Mike Schuh <mikeschuh@users.noreply.github.com>
2016-04-27 22:47:12 -0400

    updated 100_e

--- challenges/100_e/100_e.py
+++ challenges/100_e/100_e.py
@@ -16,8 +16,8 @@ def run(mat):
     
     for ri in range(rc):
         for cj in range(cc):
-            val = int(mat[ri][cj])
-            print val
+            val = mat[ri][cj]
+            print ri, cj, val
             rsums[ri] += val
             csums[cj] += val
     
@@ -26,6 +26,43 @@ def run(mat):
     
     #print rows sorted by row sum
     #print cols sorted by col sum
+    rsums_sorted = sorted(rsums)
+    print rsums_sorted
+    ro = []
+    rowmat = []
+    for ri in range(rc):
+        for rj in range(rc):
+            if rsums[ri] == rsums_sorted[rj]:
+                ro.append(rj)
+                break
+
+    print ro
+    for ri in range(rc):
+        rowmat.append(mat[ro[ri]])
+
+    print 'row sorted:'
+    print rowmat
+
+
+    co = []
+    colmat = []
+    csums_sorted = sorted(csums)
+    print csums_sorted
+    for ci in range(cc):
+        for cj in range(cc):
+            if csums[ci] == csums_sorted[cj]:
+                co.append(cj)
+                break
+    print co
+    for rj in range(rc):
+        tmp = [0] * cc
+        for ci in range(cc):
+            tmp[co[ci]] = mat[rj][ci]
+        colmat.append(tmp)
+
+    print 'col sorted:'
+    print colmat
+    
     
     
 
@@ -33,7 +70,7 @@ def readfile(fname):
     datamat = []
     with open(fname, 'r') as f:
         for line in f:
-            tokens = line.split(' ') #drop newline, then split on whitespace
+            tokens = [int(x) for x in line.split(' ')] #split on whitespace and cast as ints
             datamat.append(tokens)
     
     return datamat
