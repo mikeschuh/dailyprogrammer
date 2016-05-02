c8b59e5373254b75a221cc23f41b3ccb9597ae93
Mike Schuh <mikeschuh@users.noreply.github.com>
2016-05-02 15:15:51 -0400

    initial 89e soln

--- challenges/089e/089.py
+++ challenges/089e/089.py
@@ -1,8 +1,32 @@
 #
+import sys
+import math
 
+
+def test(data):
+
+    #mean
+    mu = sum(data) / float(len(data))
+    
+    #variance
+    var = 0
+    for v in data:
+    	var += (v - mu)**2
+    var = var / float(len(data))
+
+    #standard deviation
+    stdev = math.sqrt(var)
+    print mu, var, stdev
+    
 
 def main():
-    pass
+    
+    fstr = sys.argv[1]
+    fin = open(fstr, 'r')
+    data = [float(x) for x in fin.readlines()]
+    
+    test(data)
+    
 
 if __name__ == "__main__":
     main()
