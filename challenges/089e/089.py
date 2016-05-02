014b6bf94629ee582de936a765b6d4127f677d3c
Mike Schuh <mikeschuh@users.noreply.github.com>
2016-05-02 15:17:28 -0400

    full 89e soln

--- challenges/089e/089.py
+++ challenges/089e/089.py
@@ -3,6 +3,33 @@ import sys
 import math
 
 
+def get_mean(data):
+    
+    return sum(data) / float(len(data))
+    
+def get_variance(data):
+    
+    mu = get_mean(data)
+    
+    var = 0
+    for v in data:
+    	var += (v - mu)**2
+    var = var / float(len(data))
+
+    return var
+    
+def get_stdev(data):
+    
+    return math.sqrt(get_variance(data))
+    
+
+def get_all(data):
+    
+    mu = get_mean(data)
+    var = get_variance(data)
+    stdev = get_stdev(data)
+    return mu, var, stdev
+
 def test(data):
 
     #mean
@@ -25,7 +52,14 @@ def main():
     fin = open(fstr, 'r')
     data = [float(x) for x in fin.readlines()]
     
-    test(data)
+    #test(data)
+    
+    mu, var, stdev = get_all(data)
+    print 'mean = ', mu
+    print 'var = ', var
+    print 'stdev = ', stdev
+    
+
     
 
 if __name__ == "__main__":
