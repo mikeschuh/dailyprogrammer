aa3956dab00ff2b0308ba3b1d149736709a36379
Mike Schuh <mikeschuh@users.noreply.github.com>
2016-05-09 23:50:37 -0400

    setup 91e

--- challenges/091e/091e.py
+++ challenges/091e/091e.py
@@ -2,9 +2,23 @@
 
 import sys
 
+
+def sleep_sort(intlist):
+	pass
+
+
 def main():
-    pass
-    
+
+	if len(sys.argv) == 1:
+		print 'python 091e.py {list of ints to sort}'
+		exit()
+
+	intlist = sys.argv[1:]
+	print intlist
+
+	sleep_sort(intlist)
+
+
 if __name__ == "__main__":
     main()
     
