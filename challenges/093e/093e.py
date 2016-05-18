7d3de2d2530a1853a65ade2cce0d3993c5847137
Mike Schuh <mikeschuh@users.noreply.github.com>
2016-05-18 19:44:10 -0400

    py soln for 93e

--- challenges/093e/093e.py
+++ challenges/093e/093e.py
@@ -1,8 +1,56 @@
 #
+import sys
+
+morsecode_lookup = {'a':'.-', 'b':'-...', 'c':'-.-.', 'd':'-..',
+                    'e':'.', 'f':'..-.', 'g':'--.', 'h':'....',
+                    'i':'..', 'j':'.---', 'k':'-.-', 'l':'.-..',
+                    'm':'--', 'n':'-.', 'o':'---', 'p':'.--.',
+                    'q':'--.-', 'r':'.-.', 's':'...', 't':'-',
+                    'u':'..-', 'v':'...-', 'w':'.--', 'x':'-..-',
+                    'y':'-.--', 'z':'--..', ' ':' '}
+
+
+def morse_encode(instr):
+
+    outstr = ""
+    for let in instr:
+        tok = morsecode_lookup[let]
+        outstr = outstr + tok + ' '
+    outstr = outstr[:-1]
+    return outstr
+
+
+def morse_decode(instr):
+
+    outstr = ""
+    mkeys = morsecode_lookup.keys()
+
+    tokens = instr.split(' ')
+    for tok in tokens:
+        # hack to catch consecutive spaces, which are parsed as empty
+        # token strings and represent a true space in the string
+        if len(tok) == 0:
+            outstr = outstr + ' '
+        else:
+            for key in mkeys:
+                if tok == morsecode_lookup[key]:
+                    outstr = outstr + key
+
+    return outstr
 
 
 def main():
-    pass
+    
+    encodestr = 'sos'
+    decodestr = '... --- ...'
+    # encodestr = 'hello world'
+    # decodestr = '.... . .-.. .-.. ---   .-- --- .-. .-.. -..'
+
+    code = morse_encode(encodestr)
+    print "'%s'  =>  '%s'" % (encodestr, code)
+    text = morse_decode(decodestr)
+    print "'%s'  =>  '%s'" % (decodestr, text)
+    
 
 if __name__ == "__main__":
     main()
