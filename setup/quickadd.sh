f19512ac5ff5afb8d76028595f33535e1f4d8c1c
Mike Schuh <mikeschuh@users.noreply.github.com>
2016-05-26 20:10:20 -0400

    file header autofill in setup

--- setup/quickadd.sh
+++ setup/quickadd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# if no arguments given, display help
 if [ $# -ne 1 ]; then
     echo $0: usage: sh setup/quickadd.sh [CNC]
     # ex: sh setup/quickadd.sh 094e
@@ -9,5 +10,9 @@ fi
 name=$1
 
 mkdir -p challenges/${name}/
-touch challenges/${name}/${name}.md
+#touch challenges/${name}/${name}.md
 touch challenges/${name}/${name}.py
+
+# auto fill the common file heading
+echo "\nChallenge "${name} 1>> challenges/${name}/${name}.md
+echo "====\n" 1>> challenges/${name}/${name}.md
